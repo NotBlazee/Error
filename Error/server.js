1458f2b30355d9722b9ee132bfd7a1a3176ea6a7
Blaze <72438879+NotBlazee@users.noreply.github.com>
2020-12-16 16:47:21 +0530

    Update server.js

--- Error/server.js
+++ Error/server.js
@@ -1,9 +1,9 @@
 const discord = require("discord.js");
 const client = new discord.Client();
 const alexa = require("alexa-bot-api");
-var chatbot = new alexa("aw2plm"); //free access key for everyone [Provided by BlaZe]
+var chatbot = new alexa("aw2plm"); //free access key for everyone [Provided by Blaze]
 client.on("ready", () => {
-  console.log("Ready for chatting!| Bot by NotBlaZe#5895");
+  console.log("Ready for chatting!| Bot by 𝙱𝚕𝚊𝚣𝚎#7191");
 });
 client.on("message", async message => {
   if (message.author.bot) return;
